232bd31edb7ac765d74fe0e5fb232cab18c71942
Krasen Kochev <krasen.kochev.a50@learn.telerikacademy.com>
2023-05-20 10:26:36 +0300

    Update on BoardR from the 3rd in-home homework (Inheritance and Polymorphism)

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,10 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
-        BoardItem item1 = new BoardItem("Implement login/logout", LocalDate.now().plusDays(3));
-        BoardItem item2 = new BoardItem("Secure admin endpoints", LocalDate.now().plusDays(5));
-
-        Board board = new Board();
-
-        board.addItem(item1); // add item1
-        board.addItem(item2); // add item2
-         // do nothing - item2 already in the list
-
-        System.out.println(board.totalItems()); // count: 2
-
+        Task task = new Task("Test the application flow", "Pesho", LocalDate.now().plusDays(1));
+        task.advanceStatus();
+        task.advanceStatus();
+        task.setAssignee("Gosho");
+        task.displayHistory();
     }
 }
